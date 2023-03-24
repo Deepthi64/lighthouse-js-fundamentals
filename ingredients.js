4e630256c27b657ee1348039232e1f3fe7c9d815
deepthi vuyyuru <vdeepthi.dara@gmail.com>
2023-03-24 00:28:37 -0400

    ingredients

---
+++ ingredients.js
@@ -0,0 +1,4 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+for (  const ingredient of ingredients);
+ console.log(ingredients);
